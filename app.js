f71c638273ad8ab411276c21862e276476e083cd
likhit <mrlikhit1992@gmail.com>
2023-10-21 10:26:59 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+require('express-async-errors');
+
+// Security
+// xss-clean (req.body, req.params, req.query prevent cross site scripting)
+// express-rate-limit
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
+const express = require('express');
+const app = express();
+
+// connectDB
+const connectDB = require('./db/connect');
+const authUser = require('./middleware/authentication');
+
+// routes
+const authRoute = require('./routes/auth');
+const jobsRoute = require('./routes/jobs');
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors()); // cross origin
+app.use(xss()); // cross site script
+
+// routes
+app.get('/', (req, res) => {
+  res.send('jobs api');
+});
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/jobs', authUser, jobsRoute);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// START
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
