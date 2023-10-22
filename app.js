cef83de9c65d943c960b37b70edc275252b828a8
likhit <mrlikhit1992@gmail.com>
2023-10-22 10:10:47 +0700

    add swagger api docs

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -39,8 +44,10 @@ app.use(xss()); // cross site script
 
 // routes
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/jobs', authUser, jobsRoute);
